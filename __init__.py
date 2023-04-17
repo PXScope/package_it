95258d6449c07d3d5d374a0f4e30c132d7a7d665
swkang <swkang@pxscope.com>
2023-04-17 10:14:16 +0900

    Change overall strcuture

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
 from . import cargo
-from .core import cd_to_here, setup_args, package, setup, setup_with_args, ArgInit, init_with_args
+from .core import *
--- core.py
+++ core.py
@@ -11,89 +11,80 @@ import sys
 from typing import Callable, List, Tuple, Dict
 from pathlib import Path
 
-def cd_to_here(file, chdir_offset: str = None):
-    # Work in script directory (basically, workspace root)
+def chdir_file_path(file, chdir_offset: str = None):
+    '''
+    Changes current working directory to the directory of the file.
+    '''
     os.chdir(os.path.dirname(os.path.realpath(file)))
 
     if chdir_offset is not None:
         os.chdir(chdir_offset)
 
 class ArgInit:
-    static_field = ""
-    def __init__(self, parser: argparse.ArgumentParser) -> None:
-        parser.add_argument(
-            'prefix', help='A mandatory build type prefix string')
-        parser.add_argument(
-            '--no-archive', action='store_true',
-            help='Do not make archive')
-        parser.add_argument(
-            '--no-build', action='store_true',
-            help='Do not run build script')
-        parser.add_argument(
-            '--overwrite', action='store_true',
-            help='Overwrite exsiting archive')
-        parser.add_argument(
-            '--no-clean', action='store_true',
-            help='Do not clean up unpacked content')
-        parser.add_argument(
-            '--allow-empty-dir', action='store_true',
-            help='Allow empty directory result')
-        parser.add_argument(
-            '--version-suffix', '-V', dest='version_suffix', default=None, type=str,
-            help='A string which is suffixed after version number. e.g. "rc1"')
-        parser.add_argument(
-            '--git-tag', action='store_true',
-            help='Allow empty directory result'
-        )
-
-        args = parser.parse_args()
-        self.args = args
-        self.prefix: str = args.prefix
-        self.no_archive: bool = args.no_archive
-        self.no_build: bool = args.no_build
-        self.overwrite: bool = args.overwrite
-        self.no_clean: bool = args.no_clean
-        self.auto_git_tag: bool = args.git_tag
-        self.allow_empty_dir: bool = args.allow_empty_dir
-        self.version_suffix: str or None = args.version_suffix
-
-def init_with_args(parser: argparse.ArgumentParser) -> ArgInit:
-    global ARGINIT
-    ARGINIT = ArgInit(parser)
-    return ARGINIT
-
-def setup_args() -> str:
     '''
-    Startup script. You can skip this if you want to feed parameters directly.
-
-    :return: prefix string that was retrieved from arguments
+    Additional packaging arguments which can be acquired from argument parser
     '''
-    print(f"info: Working from directory '{os.path.abspath(os.curdir)}'...")
+    def __init__(self, parser: argparse.ArgumentParser = None) -> None:
+        if parser is not None:
+            parser.add_argument(
+                'prefix', help='A mandatory build type prefix string')
+            parser.add_argument(
+                '--no-archive', action='store_true',
+                help='Do not make archive')
+            parser.add_argument(
+                '--no-build', action='store_true',
+                help='Do not run build script')
+            parser.add_argument(
+                '--overwrite', action='store_true',
+                help='Overwrite exsiting archive')
+            parser.add_argument(
+                '--no-clean', action='store_true',
+                help='Do not clean up unpacked content')
+            parser.add_argument(
+                '--allow-empty-dir', action='store_true',
+                help='Allow empty directory result')
+            parser.add_argument(
+                '--version-suffix', '-V', dest='version_suffix', default=None, type=str,
+                help='A string which is suffixed after version number. e.g. "rc1"')
+            parser.add_argument(
+                '--git-tag', action='store_true',
+                help='Allow empty directory result'
+            )
 
-    # Parse arguments
-    init = init_with_args(argparse.ArgumentParser())
-    return init.prefix
-
-
-def setup(file: str, chdir_offset: str = None) -> str:
-    cd_to_here(file, chdir_offset)
-    return setup_args()
-
-def setup_with_args(
-        file: str,
-        parser: argparse.ArgumentParser = argparse.ArgumentParser(),
-        chdir_offset: str = None):
-    cd_to_here(file, chdir_offset)
-    return init_with_args(parser)
+            args = parser.parse_args()
+            self.args = args
+            self.prefix: str = args.prefix
+            self.no_archive: bool = args.no_archive
+            self.no_build: bool = args.no_build
+            self.overwrite: bool = args.overwrite
+            self.no_clean: bool = args.no_clean
+            self.auto_git_tag: bool = args.git_tag
+            self.allow_empty_dir: bool = args.allow_empty_dir
+            self.version_suffix: str or None = args.version_suffix
+        else:
+            self.args = None
+            self.prefix: str = ""
+            self.no_archive: bool = False
+            self.no_build: bool = False
+            self.overwrite: bool = False
+            self.no_clean: bool = False
+            self.auto_git_tag: bool = False
+            self.allow_empty_dir: bool = False
+            self.version_suffix: str or None = None
 
 class PackageResult:
-    def __init__(
-            self,
-            oname: str, pkg_dir: str) -> None:
+    '''
+    Package outputs, such as actual output file name, package directory, etc ...
+    '''
+    def __init__(self, oname: str, pkg_dir: str, version: str) -> None:
         self.oname = oname
         self.pkg_dir = pkg_dir
+        self.version = version
 
 class FileCopyFilterArgs:
+    '''
+    File copy filter arguments, which is fed to filter callback
+    '''
     def __init__(self, source_key: str, file_name: str, src_file: io.FileIO, dst_file: io.FileIO) -> None:
         self.source_key = source_key
         self.file_name = file_name
@@ -116,13 +107,7 @@ def package(
 
     copy_filters: Dict[str, Callable[[FileCopyFilterArgs], None]] = None,
 
-    no_archive: bool = False,
-    no_build: bool = False,
-    overwrite: bool = False,
-    no_clean: bool = False,
-    allow_empty_dir: bool = False,
-    auto_git_tag: bool = False,
-    version_suffix: str = None,
+    opt: ArgInit = None,
 ) -> PackageResult or None:
     """
     Run packaging script
@@ -142,28 +127,20 @@ def package(
     :return: None
     """
 
-    if 'ARGINIT' in globals():
-        global ARGINIT
-        no_archive |= ARGINIT.no_archive
-        no_build |= ARGINIT.no_build
-        overwrite |= ARGINIT.overwrite
-        no_clean |= ARGINIT.no_clean
-        allow_empty_dir |= ARGINIT.allow_empty_dir
-        auto_git_tag |= ARGINIT.auto_git_tag
-        if ARGINIT.version_suffix is not None:
-            version_suffix = ARGINIT.version_suffix
+    if args is None:
+        args = ArgInit()
 
-    version_tag = version_suffix if version_suffix is not None else ''
+    version_tag = opt.version_suffix if opt.version_suffix is not None else ''
     oname = f"{result_dir}/archive/{out_name}-{version}{version_tag}-{prefix}-{platform.system()}-{platform.release()}"
     pkg_dir = f"{result_dir}/{platform.system()}-{platform.release()}/{out_name}-{prefix}"
 
     oname_platform = oname + (".zip" if platform.system() == "Windows" else ".tar.gz")
 
-    if not overwrite and not no_archive and os.path.exists(oname_platform):
+    if not opt.overwrite and not opt.no_archive and os.path.exists(oname_platform):
         raise Exception(f'fatal: File already exists: {oname}')
 
     # 1.2. Run build
-    if not no_build and build_callback is not None:
+    if not opt.no_build and build_callback is not None:
         r = build_callback()
         if 0 != r:
             raise Exception(f'fatal: Build script returned error: {r}')
@@ -171,7 +148,7 @@ def package(
     # 2.0 Collect all files inside package directory
     non_targets = set([])
 
-    if not no_clean:
+    if not opt.no_clean:
         for root, subdirs, files in os.walk(pkg_dir):
             for file in files:
                 non_targets.add(os.path.abspath(os.path.join(root, file)))
@@ -269,7 +246,7 @@ def package(
         os.remove(non_target)
 
     # 3.2. Remove empty directories
-    if not allow_empty_dir:
+    if not opt.allow_empty_dir:
         for root, _, _ in os.walk(pkg_dir, topdown=False):
             if len(os.listdir(root)) == 0:
                 print(f"-- Remove empty: {os.path.relpath(root, os.path.abspath(os.curdir))}")
@@ -287,10 +264,11 @@ def package(
     # 4. Zip packaged archive
     retval = PackageResult(
         oname=oname_platform,
-        pkg_dir=pkg_dir
+        pkg_dir=pkg_dir,
+        version=version,
     )
 
-    if no_archive:
+    if opt.no_archive:
         print('info: skipping archive creation ... ')
         return retval
 
@@ -302,7 +280,6 @@ def package(
         "zip" if platform.system() == "Windows" else "gztar",
         pkg_dir
     )
-    print(f"done.")
 
     if package_dir_callback:
         package_dir_callback(pkg_dir)
@@ -318,9 +295,9 @@ def package(
         except Exception as e:
             print(f"error {e}")
 
-    if auto_git_tag:
+    if opt.auto_git_tag:
         print(f"info: tagging git repository with {version}{version_tag} ... ", end='', flush=True)
         subprocess.run(['git', 'tag', f'v{version}{version_tag}'])
-        print(f"done.")
 
+    print(f"done.")
     return retval
